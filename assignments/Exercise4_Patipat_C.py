c1410aa0ce2a2c7c73dca813c72e2234dab644c7
patifiat <72874539+patifiat@users.noreply.github.com>
2020-10-17 02:35:24 +0700

    Exercise4

---
+++ assignments/Exercise4_Patipat_C.py
@@ -0,0 +1,11 @@
+####Exercise 4###
+FoundEng = 65.0     
+GenBus = 23.0     
+IntroComSys = 30.0   
+ComPr = 75.50
+
+print("--- Your Score ---")
+print("Foundation English: ", FoundEng)
+print("General Business: ", GenBus)
+print("Introduction to Computer Systems: ", IntroComSys)
+print("Computer Programming: ", ComPr)
